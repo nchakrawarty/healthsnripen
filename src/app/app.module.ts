6e951b3466990f8634be7dfbcab0a643df403849
Swati <swati@Swatis-MacBook-Air.local>
2019-08-26 07:49:04 +0530

    working tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,14 +9,21 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HealthKit } from "@ionic-native/health-kit/ngx";
+import { HttpModule } from "@angular/http";
+import { HttpClientModule } from "@angular/common/http";
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpModule,
+    HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    HealthKit
   ],
   bootstrap: [AppComponent]
 })
